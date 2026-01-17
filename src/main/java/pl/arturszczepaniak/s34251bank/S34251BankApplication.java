bde9704baabaac1ee578a34cb94fc2b62050e5c4
artchieas720 <184821652+artchieas720-uni@users.noreply.github.com>
2026-01-17 12:56:46 +0100

    add

---
+++ src/main/java/pl/arturszczepaniak/s34251bank/S34251BankApplication.java
@@ -0,0 +1,13 @@
+package pl.arturszczepaniak.s34251bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S34251BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S34251BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/arturszczepaniak/s34251bank/S34251BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arturszczepaniak.s34251bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S34251BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
